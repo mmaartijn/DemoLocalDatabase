0f0b89bb8ed67b69bbd255892caeb47341ae68c5
mmaartijn <mmaartijn@gmail.com>
2015-01-19 11:55:37 +0100

    SQLite

--- www/js/index.js
+++ www/js/index.js
@@ -3,68 +3,62 @@ var db;
 $(document).ready(function(){
     document.addEventListener('deviceready', onDeviceReady, false);
 
-    bindButtons();
-});
-
-function onDeviceReady(){
-    $('.deviceNotReady').removeClass('deviceNotReady').addClass('deviceReady');
-    
-    // Open the database.
-    db = window.openDatabase("dbName", "1.0", "DB Displayname", 1000000);
-    db.transaction(createPersonsTable, errorCB);
-}
-
-// All in one transaction
-function createPersonsTable(trans) {
-    trans.executeSql('CREATE TABLE IF NOT EXISTS Person (name, age)');
-    getAllPersons(trans);
-}
-
-function getAllPersons(trans) {
-    trans.executeSql('SELECT * FROM Person', [], getAllPersonsCB, errorCB);
-}
-
-function getAllPersonsCB(trans, results){
-    var htmlRows = '';
-    var len = results.rows.length;
-    for (var i=0; i<len; i++){
-        htmlRows += '<tr><td>' + results.rows.item(i).name+ '</td><td>' + results.rows.item(i).age + '</td></tr>';
-    }
-
-    $('#results tbody').html(htmlRows);
-}
-
-function bindButtons(){
     $('#btnSubmit').click(btnSubmitClicked);
     $('#btnClear').click(btnClearClicked);
-}
+});
 
 function btnSubmitClicked(){
     var name = $('#txtName').val();
     var age = $('#txtAge').val();
 
-    if(db){
-        db.transaction(function(trans){ 
-            addPerson(trans, name, age);
-        }, errorCB);
+    addPerson(name, age);
 
-        $('#txtName').val('');
-        $('#txtAge').val('');
-    }
+    $('#txtName').val('');
+    $('#txtAge').val('');
 }
 
 function btnClearClicked(){
-    if(db){
-        db.transaction(function(trans){ 
-            trans.executeSql('DELETE FROM Person');
-            getAllPersons(trans);
-        }, errorCB);
-    }
+    db.transaction(function(trans){ 
+        trans.executeSql('DELETE FROM Person');
+        getAllPersons(trans);
+    }, errorCB);
 }
 
-function addPerson(trans, name, age){
-    trans.executeSql('INSERT INTO Person (name, age) VALUES (?,?)', [ name, age ]);
-    getAllPersons(trans);
+function onDeviceReady(){
+    $('.deviceNotReady').removeClass('deviceNotReady').addClass('deviceReady');
+    
+    // Open the database.
+    db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
+
+    initializeDatabase();
+    
+}
+
+// All in one transaction
+function initializeDatabase() {
+    db.transaction(function(trans){ 
+        trans.executeSql('CREATE TABLE IF NOT EXISTS Person (name, age)');
+        getAllPersons(trans);
+    }, errorCB);
+}
+
+function getAllPersons(trans) {
+    trans.executeSql('SELECT * FROM Person', [], function(trans, results){
+        var htmlRows = '';
+        var len = results.rows.length;
+        for (var i=0; i<len; i++){
+            htmlRows += '<tr><td>' + results.rows.item(i).name+ '</td><td>' + results.rows.item(i).age + '</td></tr>';
+        }
+
+        $('#results tbody').html(htmlRows);
+    }, errorCB);
+}
+
+function addPerson(name, age){
+    db.transaction(function(trans){ 
+        trans.executeSql('INSERT INTO Person (name, age) VALUES (?,?)', [ name, age ]);
+        getAllPersons(trans);
+    }, errorCB);
 }
 
 // Transaction error callback
