b5d9e09309a34fdbee56333dc9dbef696deb6c53
mmaartijn <mmaartijn@gmail.com>
2015-01-19 10:04:51 +0100

    Javascript error weggehaald. Clear button gemaakt.

--- www/js/index.js
+++ www/js/index.js
@@ -9,58 +9,65 @@ $(document).ready(function(){
 function onDeviceReady(){
     $('.deviceNotReady').removeClass('deviceNotReady').addClass('deviceReady');
     db = window.openDatabase("MyDatabaseName", "1.0", "My database display name", 200000 /* Size */);
-    db.transaction(createPersonsTable, errorCB, successCB);
-}
-
-function bindButtons(){
-    $('#btnSubmit').click(function(){
-        var name = $('#txtName').val();
-        var age = $('#txtAge').val();
-
-        if(db){
-            db.transaction(function(trans){
-                addPerson(trans, name, age);
-            }, errorCB, successCB);
-        }
-    });
-}
-
-function addPerson(trans, name, age){
-    trans.executeSql('INSERT INTO Person (name, age) VALUES ("' + name + '", "' + age + '")');
+    db.transaction(createPersonsTable, errorCB);
 }
 
 // All in one transaction
 function createPersonsTable(trans) {
-    trans.executeSql('DROP TABLE IF EXISTS Person');
+    //trans.executeSql('DROP TABLE IF EXISTS Person');
     trans.executeSql('CREATE TABLE IF NOT EXISTS Person (name, age)');
+    getAllPersons(trans);
+}
+
+function getAllPersons(trans) {
+    trans.executeSql('SELECT * FROM Person', [], getAllPersonsCB, errorCB);
+}
+
+function getAllPersonsCB(trans, results){
+    var htmlRows = '';
+    var len = results.rows.length;
+    for (var i=0; i<len; i++){
+        htmlRows += '<tr><td>' + results.rows.item(i).name+ '</td><td>' + results.rows.item(i).age + '</td></tr>';
+    }
+
+    if(htmlRows.length){
+        $('#results tbody').html(htmlRows);
+    }
+}
+
+function bindButtons(){
+    $('#btnSubmit').click(btnSubmitClicked);
+    $('#btnClear').click(btnClearClicked);
+}
+
+function btnSubmitClicked(){
+    var name = $('#txtName').val();
+    var age = $('#txtAge').val();
+
+    if(db){
+        db.transaction(function(trans){ 
+            addPerson(trans, name, age);
+        }, errorCB);
+    }
+}
+
+function btnClearClicked(){
+    if(db){
+        db.transaction(function(trans){ 
+            trans.executeSql('DELETE FROM Person', [], undefined, errorCB);
+        }, errorCB);
+    }
+}
+
+function addPerson(trans, name, age){
+    trans.executeSql('INSERT INTO Person (name, age) VALUES (?,?)', [ name, age ]);
+    getAllPersons(trans);
 }
 
 // Transaction error callback
 //
 function errorCB(err) {
     alert("Error processing SQL: " + err.message);
-}
 
-// Transaction success callback
-//
-function successCB() {
-    db.transaction(function(trans){
-        trans.executeSql('SELECT * FROM Person', [], allPersonsSelected, errorCB);
-    }, errorCB);
-
-    return false;
-}
-
-function allPersonsSelected(transaction, results){
-    var htmlRows = '';
-    var len = results.rows.length;
-    for (var i=0; i<len; i++){
-        htmlRows += '<tr><td>' + rows.item(i).name+ '</td><td>' + rows.item(i).age + '</td></tr>';
-    }
-
-    if(htmlRows.length){
-        $('#results tbody').html(htmlRows);
-    }
-
-    return false;
+    return true;
 }
